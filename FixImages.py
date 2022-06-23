5040983f58517e1bf865c5e6f0f5c43fe7c140b5
Daniel Koman <dankoman30@gmail.com>
2022-06-23 16:37:29 -0700

    add PLZ archive support

--- FixImages.py
+++ FixImages.py
@@ -1,36 +1,79 @@
 #!/usr/local/bin/python
-import os, fnmatch, subprocess, sys
+import os, fnmatch, subprocess, shutil, zipfile
+from zipfile import ZipFile
 
-# =====================================================================================================================
-# FUNCTIONS:
-# =====================================================================================================================
-def findReplace(directory, find, replace): # this function takes directory path, string to find, and string to replace with, as parameters
-    filePattern = "*.svg"
+def decompressPLZs(directory): # this function decompresses all PLZs in given directory into same directory
+    
+    temp_directory = os.path.abspath(directory + "/FixImages_temp") # define temporary directory name
+    new_png_directory = os.path.abspath(temp_directory + "/new_png") # define new_png temporary directory name
+
+    # Extract all the contents of zip file in temporary subdirectory
     for path, dirs, files in os.walk(os.path.abspath(directory)): # walk through directory and file structure in predefined path to find files
-        for filename in fnmatch.filter(files, filePattern): # iterate through only the file that match specified extension
-            filepath = os.path.join(path, filename) # join path and filename to get absolute file path
-            with open(filepath) as f:
-                s = f.read() # open the file
-            s = s.replace(find, replace) # replace instances of 'find' string with 'replace' string
-            with open(filepath, "w") as f:
-                f.write(s) # close the file
+        for plzFileName in fnmatch.filter(files, "*.plz"): # iterate through only the file that match plz file extension
+            plzFilePath = os.path.join(path, plzFileName) # join path and filename to get absolute file path
+            with ZipFile(plzFilePath, 'r') as archive:
+                archive.extractall(temp_directory)
+        break # prevent descending into subfolders
 
-def mogrify(directory): # this function runs mogrify to write the svg (with embedded png) as new raster image
-                        # IMPORTANT: IT'S ASSUMED THAT MOGRIFY (PART OF IMAGEMAGICK) IS ALREADY INSTALLED!!!
-    process = subprocess.Popen(f'mogrify -path C:/new_png -format png {directory}/*.svg', # run the shell command
+    # now that all files are extracted, let's find and replace
+    for path, dirs, files in os.walk(os.path.abspath(temp_directory)): # walk through temp_directory to find files
+        for svgFileName in fnmatch.filter(files, "*.svg"): # iterate through only the file that match specified extension
+            svgFilePath = os.path.join(path, svgFileName) # join path and filename to get absolute file path
+            with open(svgFilePath) as f:
+                s = f.read() # open the file
+            s = s.replace('stroke=\"#FFFFFF\" stroke-width=\"2\" ', '') # find and replace
+            s = s.replace('<text', '<text font-size=\"36px\" dy=\"9px\"') # find and replace
+            with open(svgFilePath, "w") as f:
+                f.write(s) # close the file
+        break # prevent descending into subfolders
+
+    # now we need to mogrify
+    if not os.path.exists(new_png_directory):
+        print(f'creating directory {new_png_directory}')
+        os.makedirs(new_png_directory) # create new directory (because mogrify is dumb and can't create one itself)
+
+    process = subprocess.Popen(f'mogrify -path {new_png_directory} -format png {temp_directory}/*.svg', # run the shell command
                            shell=True, stdout=subprocess.PIPE)
     process.wait() # wait for process to finish in current thread before proceeding
 
-# =====================================================================================================================
-# =====================================================================================================================
+    # we need to remove the original png source images from the plz archives to avoid duplication
+    for path, dirs, files in os.walk(os.path.abspath(directory)): # walk through directory and file structure in predefined path to find files
+        for plzFileName in fnmatch.filter(files, "*.plz"): # iterate through only the file that match plz file extension
+            old_filepath = os.path.join(path, plzFileName)
+            new_filepath = os.path.join(path, plzFileName + "_new")
+
+            zin = zipfile.ZipFile(old_filepath, 'r') # old file
+            zout = zipfile.ZipFile(new_filepath, 'w') # new file
+            for item in zin.infolist():
+                buffer = zin.read(item.filename)
+                if (item.filename[-4:] != '.png'): # write all except png files to new archive
+                    zout.writestr(item, buffer)
+            zout.close() # close files
+            zin.close() # close files
+            os.remove(old_filepath) # delete old file
+            os.rename(new_filepath, old_filepath) # rename new file to old filename
+        break # prevent descending into subfolders
 
 
+    # now we need to re-pack the new png files into their original archives
+    for path, dirs, files in os.walk(os.path.abspath(directory)): # walk through directory and file structure in predefined path to find files
+        for plzFileName in fnmatch.filter(files, "*.plz"): # iterate through only the file that match plz file extension
 
+            plzFilePath = os.path.join(path, plzFileName) # join path and filename to get absolute file path
+            pngFileName = plzFileName.replace('.plz', '.png')
+            pngFilePath = os.path.join(new_png_directory, pngFileName) # remove last 3 characters from plz file path (.plz) to get base filename (no extension).
+                                                                                                      # Then, append "png" to it to get the png file path (after joining with new_png_directory path)
 
+            with ZipFile(plzFilePath, 'a') as archive: # append
+                archive.write(pngFilePath, arcname=pngFileName) # write the new png file to the archive
+                archive.printdir()
+        break # prevent descending into subfolders
 
-# =====================================================================================================================
-# MAIN PROGRAM BODY
-# =====================================================================================================================
+    # clean up (delete temporary directory)
+    cleanup = input("Cleanup temporary files? Type YES to delete.")
+    if cleanup.upper() == "YES":
+        shutil.rmtree(temp_directory)
+
 print("Welcome to Documoto image fixer!")
 print("This is an application to embed Documoto callout bubbles onto the source raster images.")
 print("First, extract the svg and png files from plz archives into desired directory.")
@@ -42,8 +85,7 @@ print("and can be deleted after this process.")
 
 directory = input("Enter directory containing png and svg files:")
 directory = directory.replace('\\', '/') # replace backslashes with forward slashes
-findReplace(directory, 'stroke=\"#FFFFFF\" stroke-width=\"2\" ', '') # remove while outlines from bubbles
-findReplace(directory, '<text', '<text font-size=\"36px\" dy=\"9px\"') # increase text size and center text in bubble
-mogrify(directory)
+decompressPLZs(directory)
+
 
 print("GOODBYE!")
