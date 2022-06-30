a8458e50ebb6169425f2edf321013a8fc0536284
Daniel Koman <dankoman30@gmail.com>
2022-06-30 16:13:25 -0700

    check directory structure for spaces so mogrify doesn't fail

--- FixImages.py
+++ FixImages.py
@@ -153,23 +153,24 @@ print("")
 print("Welcome to Documoto Image Fixer!")
 print("================================")
 print("This is an application to embed Documoto callout bubbles onto the source raster images.")
-print("First, extract the svg and png files from plz archives into desired directory.")
-print("The directory entered below must include documoto vector images (svg) and")
-print("their corresponding raster images (png). The resulting new raster images (png)")
-print("will be located in C:\\new_png, and must be re-packaged into the Documoto package (plz)")
-print("The original decompressed png and svg files are only used to generate the new raster images")
-print("and can be deleted after this process, or you can elect to delete them during the")
-print("conversion process.")
+print("The directory entered below must include documoto package files (plz).")
+print("Files will be extracted, modifications made, and repackaged into the original archives.")
 print("")
 
 isValidDirectory = False
 
 while not isValidDirectory:
-    directory = input("Enter full directory containing png and svg files: ")
+    directory = input("Enter full directory containing the plz files: ")
     directory = directory.replace('\\', '/') # replace backslashes with forward slashes
     print("")
     if os.path.exists(directory): # check to see if directory exists before proceeding
-        isValidDirectory = True # if valid directory, flag this to true to prevent next loop iteration
+        if not ' ' in directory: # check to see if directory has spaces
+            isValidDirectory = True # if no spaces, flag this to true to prevent next loop iteration
+        else: # complain
+            print("")
+            print(f"{directory} contains spaces. Please use a directory structure containing no spaces.")
+            print("Please try again!")
+            print("")
     else: # complain
         print("")
         print(f"{directory} is not a valid path.")
